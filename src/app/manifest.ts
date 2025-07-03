3699e9c8675c6764acd7482c4991b69a2eba8993
piro0919 <piro.haniwa@gmail.com>
2025-07-03 23:54:55 +0900

    feat(manifest): set japanese metadata and remove translation functionality
    - Updated app description to "ぺらいちは、シンプルなメモサービスです。"
    - Set language to Japanese and app name to "ぺらいち"
    - Commented out code related to translation functionality

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,12 +1,44 @@
-import { getTranslations } from "next-intl/server";
+// import { getTranslations } from "next-intl/server";
+// import type { MetadataRoute } from "next";
+
+// export default async function manifest(): Promise<MetadataRoute.Manifest> {
+//   const t = await getTranslations({ locale: "en", namespace: "Metadata" });
+
+//   return {
+//     background_color: "#FFFFFF",
+//     description: t("description"),
+//     display: "standalone",
+//     icons: [
+//       {
+//         purpose: "maskable",
+//         sizes: "192x192",
+//         src: "/android-chrome-192x192.png",
+//         type: "image/png",
+//       },
+//       {
+//         purpose: "maskable",
+//         sizes: "512x512",
+//         src: "/icon-512x512.png",
+//         type: "image/png",
+//       },
+//     ],
+//     id: "/",
+//     lang: "en",
+//     name: t("title"),
+//     orientation: "portrait",
+//     scope: "/",
+//     short_name: t("title"),
+//     start_url: "/",
+//     theme_color: "#FFFFFF",
+//   };
+// }
+
 import type { MetadataRoute } from "next";
 
-export default async function manifest(): Promise<MetadataRoute.Manifest> {
-  const t = await getTranslations({ locale: "en", namespace: "Metadata" });
-
+export default function manifest(): MetadataRoute.Manifest {
   return {
     background_color: "#FFFFFF",
-    description: t("description"),
+    description: "ぺらいちは、シンプルなメモサービスです。",
     display: "standalone",
     icons: [
       {
@@ -23,11 +55,11 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
       },
     ],
     id: "/",
-    lang: "en",
-    name: t("title"),
+    lang: "ja",
+    name: "ぺらいち",
     orientation: "portrait",
     scope: "/",
-    short_name: t("title"),
+    short_name: "ぺらいち",
     start_url: "/",
     theme_color: "#FFFFFF",
   };
